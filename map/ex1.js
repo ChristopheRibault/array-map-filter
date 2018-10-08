133f25b82c99dfb82531802026fa39f499daf5b5
ChristopheRibault <c.ribault.dev@gmail.com>
2018-10-08 08:44:20 +0200

    map/ex1

--- map/ex1.js
+++ map/ex1.js
@@ -22,7 +22,10 @@ En sortie: [
  */
 
 function getStringsLength(strings) {
+  return strings.map(x=>`${x} contains ${x.length} characters`)
 }
+console.log(getStringsLength(['Cat', 'Lion', 'Tiger', 'Jaguar', 'Leopard']))
+
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
 // mais alors, il faut alors lancer l'exercice avec node map/ex1
