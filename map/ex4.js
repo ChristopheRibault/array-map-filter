0cbaba4379adf8cf92c4ea3fed36f4571930e8bd
ChristopheRibault <c.ribault.dev@gmail.com>
2018-10-08 10:06:03 +0200

    map/ex4

--- map/ex4.js
+++ map/ex4.js
@@ -52,6 +52,13 @@ Tableau de sortie correspondant (toFixed transforme les nombres en chaînes)
 */
 
 function getCirclesProperties(radiuses) {
+  return radiuses.map(x=>{
+    y={};
+    y['radius'] = x;
+    y['circumference'] = (2*Math.PI*x).toFixed(3);
+    y['surface'] = (Math.PI*x*x).toFixed(3);
+    return y;
+  })
 }
 
 
